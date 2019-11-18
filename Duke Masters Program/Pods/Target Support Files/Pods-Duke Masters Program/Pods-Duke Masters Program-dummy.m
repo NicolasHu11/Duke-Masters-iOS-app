2d72531a7b5cef77222b9af9c02924b3565fde0a
Yijia Hu <yh217@duke.edu>
2019-11-17 21:48:07 -0500

    deleting everything from master in order to merge

--- Duke Masters Program/Pods/Target Support Files/Pods-Duke Masters Program/Pods-Duke Masters Program-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Duke_Masters_Program : NSObject
-@end
-@implementation PodsDummy_Pods_Duke_Masters_Program
-@end
--- README.md
+++
@@ -1,5 +0,0 @@
-# Duke Masters Program
-
-- Group Memebers: Yijia Hu, Fan Zhang, Xiaochen Zhou, Yilun Sun 
-- ECE 564, Mobile App
-- Duke University, 2019 Fall
