8317b97c3197247626292f6d2c4f577e62f3637d
Yijia Hu <yh217@duke.edu>
2019-12-05 21:12:47 -0500

    fix up the directory view

--- Duke Masters Program/Duke Masters Program/Directory/DirectoryTableVC.swift
+++ Duke Masters Program/Duke Masters Program/Directory/DirectoryTableVC.swift
@@ -11,15 +11,22 @@ import UIKit
 class DirectoryTableVC: UITableViewController,UISearchBarDelegate {
     
     @IBOutlet weak var SearchBar: UISearchBar!
+    
+    
     var student_dict =  [String:String]()
     var studentarray = [[String]]()
     var Result = [[String]]()
     var signal_write = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.SearchBar.delegate = self
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        // change nav bar color
+    
+        
         let path = Bundle.main.path(forResource: "Fall 2019 MEM Students_MStxt", ofType: "txt")
         let filemagr = FileManager.default
         if filemagr.fileExists(atPath: path!){
@@ -45,10 +52,12 @@ class DirectoryTableVC: UITableViewController,UISearchBarDelegate {
                 print("Error:\(error)")
             }
         }
+        //TODO: re-order the student array
+        
+        
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -84,15 +93,21 @@ class DirectoryTableVC: UITableViewController,UISearchBarDelegate {
         }
         let first = student_Info[0]
         let last = student_Info[1]
-        cell.Cell_Label?.text = first + " " + last
+        
+        // Cell labels
+        // adding a space for alignment
+        cell.Cell_Label?.text = " " + first + " " + last
+        
 //        let Email = student_Info[2]
-        let Term =  student_Info[3]
-        let proj =  student_Info[4]
-        let MS_MEN =  student_Info[5]
-        // Configure the cell...
-        cell.Cell_Description?.text = "\(Term), \(MS_MEN) student from \(proj)"
-        // pic info
-//        cell.Cell_Image.image = UIImage(contentsOfFile: "bluedevils")
+        let Term =  student_Info[3].trimmingCharacters(in: .whitespacesAndNewlines)
+        let proj =  student_Info[4].trimmingCharacters(in: .whitespacesAndNewlines)
+        let MS_MEN =  student_Info[5].trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Configure the cell description
+        cell.Cell_Description?.text = "\(Term)\n\(proj) \(MS_MEN) "
+        
+//        // pic info, same pics for all
+//        cell.Cell_Image.image = UIImage(contentsOfFile: "BlueDevil")
         // copy current student info
         return cell
     }
@@ -113,7 +128,7 @@ class DirectoryTableVC: UITableViewController,UISearchBarDelegate {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     */
 
@@ -131,6 +146,7 @@ class DirectoryTableVC: UITableViewController,UISearchBarDelegate {
         return true
     }
     */
+    
     // MARK: unwind segue and reload data
     @IBAction func unwindPersonListTableView(segue: UIStoryboardSegue){
         // When returned from fromer segue
@@ -138,7 +154,6 @@ class DirectoryTableVC: UITableViewController,UISearchBarDelegate {
     }
     
      //MARK: - Navigation
-
 //     In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             if(segue.identifier == "toinformation") {
@@ -164,29 +179,49 @@ class DirectoryTableVC: UITableViewController,UISearchBarDelegate {
         }
         
     
-// Search Bar
-func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-    print("text input:", searchText)
+    // MARK: Search Bar
+    // TODO: add more advanced search function
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        print("text input:", searchText)
 
-    if searchText == "" {
-        Result = studentarray
-        signal_write = false
-    }
-    else { //
-        signal_write = true
-        self.Result = []
-        for student in studentarray {
-            let studentname = student[0] + " " + student[1]
-            if studentname.lowercased().hasPrefix(searchText.lowercased()) {
-                Result.append(student)
+        if searchText == "" {
+            Result = studentarray
+            signal_write = false
+        }
+        else { //
+            signal_write = true
+            self.Result = []
+            for student in studentarray {
+                // Nicolas: adding more on this
+                let studentname = student[0] + " " + student[1]
+                // check prefix
+                if studentname.lowercased().hasPrefix(searchText.lowercased()) {
+                    Result.append(student)
+                }
+                // contains
+                if studentname.lowercased().contains(searchText.lowercased()){
+                    Result.append(student)
+                }
+                // if there's space in search txt
+                if searchText.contains(" ") {
+                    // separate them
+                    let searchArray = searchText.lowercased().components(separatedBy: " ")
+                    if student[0].lowercased().contains(searchArray[0]) || student[1].lowercased().contains(searchArray[1]) {
+                        Result.append(student)
+                    }
+                    else if student[0].lowercased().contains(searchArray[1]) || student[1].lowercased().contains(searchArray[0]) {
+                        Result.append(student)
+                    }
+                }
+
+                
             }
         }
+//        if Result.count>0{
+//            print("search result 0: input", Result[0])
+//        }
+        self.tableView.reloadData()
     }
-    if Result.count>0{
-        print("search result 0: input", Result[0])
-    }
-    self.tableView.reloadData()
-}
 
     
 
